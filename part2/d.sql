c0c9c75de5116de3827f069870404006c4d45cfc
TheWerbinator <34259061+TheWerbinator@users.noreply.github.com>
2023-02-25 11:56:59 -0700

    Solve exercises
    Add code to solve DB exercises, added to changed files.

--- part2/d.sql
+++ part2/d.sql
@@ -1,4 +1,10 @@
 -- Below is a dummy query, if we leave it empty then the SQLITE package will throw a non-readable error
 -- Change the query to pass the test :)
-select *
-from dogs;
+select 
+  users.first_name, 
+  users.last_name
+from 
+  users
+  join favorites 
+    on dog_id = 2 and user_id = users.id
+    ;
